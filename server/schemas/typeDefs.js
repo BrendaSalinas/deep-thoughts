895766266b2f316afccbeb064fbcd4f9db33aacc
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-03-28 20:01:30 -0500

    Create and integrate the Reaction Type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,16 +3,26 @@ const { gql } = require('apollo-server-express');
 
 //create our typeDefs 
 const typeDefs = gql`
-    type Thought {
-        _id: ID
-        thoughtText: String
-        createdAt: String
-        username: String
-        reactionCount: Int
-    }
-    type Query {
-        thoughts(username: String): [Thought]
-    }
+
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    thoughts(username: String): [Thought]
+  }
 `;
 
 //export the typeDefs
